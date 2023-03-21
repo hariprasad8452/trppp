3e33f53a7fa9e075ccb5c833b106783b8207afdf
root <root@ip-172-31-51-17.ec2.internal>
2023-03-21 14:39:55 +0000

    commit-2

--- hari.tf
+++ hari.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
 resource "aws_instance" "one" {
-ami="ami-05afd67c4a44cc983"
-instance_type="t2.micro"
+  ami           = "ami-0d81306eddc614a45"
+  instance_type = "t2.micro"
 tags = {
-Name = "harry"
+Name = "hari"
 }
 }
